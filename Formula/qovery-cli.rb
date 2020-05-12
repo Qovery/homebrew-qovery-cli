e981b6ada8a8793e6dc995083d4c58544c1dce72
qovery <contact@qovery.com>
2020-05-12 10:23:56 +0200

    Brew formula update for qovery-cli version v0.26.2

--- Formula/qovery-cli.rb
+++ Formula/qovery-cli.rb
@@ -2,16 +2,16 @@
 class QoveryCli < Formula
   desc "Deploy modern application in seconds"
   homepage "https://docs.qovery.com"
-  version "0.26.1"
+  version "0.26.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/Qovery/qovery-cli/releases/download/v0.26.1/qovery-cli_0.26.1_darwin_amd64.tar.gz"
-    sha256 "25bf6987ce2af25bcaeecd758d7e99a87a6e7fa5f98ecef7011bb00889953db1"
+    url "https://github.com/Qovery/qovery-cli/releases/download/v0.26.2/qovery-cli_0.26.2_darwin_amd64.tar.gz"
+    sha256 "684b126d69c38b650b443a524e6f85a689333fff675fb40c2d3aa63d603b2bbb"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/Qovery/qovery-cli/releases/download/v0.26.1/qovery-cli_0.26.1_linux_amd64.tar.gz"
-      sha256 "815b2cad3f86bcdab0f5001aa58352ec602aa07269d6e346c5959424cf2f4856"
+      url "https://github.com/Qovery/qovery-cli/releases/download/v0.26.2/qovery-cli_0.26.2_linux_amd64.tar.gz"
+      sha256 "0e626638b71c984060fbd71979ab7174dedeb51ff471a77160f6cc5c2fb60a8f"
     end
   end
 
