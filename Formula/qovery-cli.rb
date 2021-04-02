235d4c28c5a046b284c7e43dd56a552ca55f07ff
qovery <contact@qovery.com>
2021-04-02 15:29:55 +0200

    Brew formula update for qovery-cli version v0.33.19

--- Formula/qovery-cli.rb
+++ Formula/qovery-cli.rb
@@ -5,24 +5,24 @@
 class QoveryCli < Formula
   desc "Deploy modern application in seconds"
   homepage "https://docs.qovery.com"
-  version "0.33.18"
+  version "0.33.19"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Qovery/qovery-cli/releases/download/v0.33.18/qovery-cli_0.33.18_darwin_amd64.tar.gz"
-    sha256 "3a53d0049c47918ef6b9fb65475dc77ea77069b734fd155a56970fa85b0717c2"
+    url "https://github.com/Qovery/qovery-cli/releases/download/v0.33.19/qovery-cli_0.33.19_darwin_amd64.tar.gz"
+    sha256 "0d024a9b771536f761b90087c8fcf1ae32621ce681a154a6c3b8248828f019ac"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Qovery/qovery-cli/releases/download/v0.33.18/qovery-cli_0.33.18_darwin_arm64.tar.gz"
-    sha256 "a94b759732289b1ab2ca6ad426092344b6935f3cb73701898b38e57702efb42e"
+    url "https://github.com/Qovery/qovery-cli/releases/download/v0.33.19/qovery-cli_0.33.19_darwin_arm64.tar.gz"
+    sha256 "18d6731d815c82a76bbe06f11bd2a1bbdbe230c855f97f946a53228bf17bac2d"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/Qovery/qovery-cli/releases/download/v0.33.18/qovery-cli_0.33.18_linux_amd64.tar.gz"
-    sha256 "898f6a6d0d4f4bd63826327a4ff802229da50400f417ec244f341d46f3f608b2"
+    url "https://github.com/Qovery/qovery-cli/releases/download/v0.33.19/qovery-cli_0.33.19_linux_amd64.tar.gz"
+    sha256 "547956fb817fcebecd4e71a85476cbbd500e8ff3bef01ae047c0b0139238a37c"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/Qovery/qovery-cli/releases/download/v0.33.18/qovery-cli_0.33.18_linux_arm64.tar.gz"
-    sha256 "c0d1027e699126eb18d1d0a2a26b4bbb98def9bf73203cdcafdc19e24903a5f7"
+    url "https://github.com/Qovery/qovery-cli/releases/download/v0.33.19/qovery-cli_0.33.19_linux_arm64.tar.gz"
+    sha256 "94ca8c00ec0423ea3c0b28bc484a8689a73759a1f9ed3879d17e565ee5eb9b39"
   end
 
   def install
