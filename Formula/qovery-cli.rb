85193d3b57dd5371d53da9000dc7f34a6494d8e7
qovery <contact@qovery.com>
2023-01-31 17:52:27 +0100

    Brew formula update for qovery-cli version v0.48.6

--- Formula/qovery-cli.rb
+++ Formula/qovery-cli.rb
@@ -10,7 +10,7 @@ class QoveryCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Qovery/qovery-cli/releases/download/v0.48.6/qovery-cli_0.48.6_darwin_arm64.tar.gz"
-      sha256 "6177d436108e2e1d0091420d12935eb286815a6ec43a1c23278d06d56691a487"
+      sha256 "823a066a8c179f2486772d5779383720ae3b6a2cf525acc32f633b2b7f15025c"
 
       def install
         bin.install "qovery"
@@ -18,7 +18,7 @@ class QoveryCli < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/Qovery/qovery-cli/releases/download/v0.48.6/qovery-cli_0.48.6_darwin_amd64.tar.gz"
-      sha256 "392e940ff91c2462a3148788bfddf1cabf658c8f0ad0e5c79beb92690443732d"
+      sha256 "98515b9997946cba0b2d33ed4074b55b8c56cdb1c23f1806d257f2156dc0c3e6"
 
       def install
         bin.install "qovery"
@@ -29,7 +29,7 @@ class QoveryCli < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/Qovery/qovery-cli/releases/download/v0.48.6/qovery-cli_0.48.6_linux_amd64.tar.gz"
-      sha256 "8af5a0cec71816d2dce331200fcff05ec62644db6d546714fed5b27d3da0154e"
+      sha256 "5d2c9e6fae38c03707bfe33df554c69409464925c59730ea4089c974fef8f561"
 
       def install
         bin.install "qovery"
@@ -37,7 +37,7 @@ class QoveryCli < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/Qovery/qovery-cli/releases/download/v0.48.6/qovery-cli_0.48.6_linux_arm64.tar.gz"
-      sha256 "a3bf13a16b1031ee4de4c2449534e42c3ef08f060a008fca7977cd0688fb8b93"
+      sha256 "71baa6d71519c95bf8f80849ab7992b344f793cce94482b8470c34333c28daf6"
 
       def install
         bin.install "qovery"
