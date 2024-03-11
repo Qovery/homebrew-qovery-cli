1a150be21dac28b9fc99aef89dca625ac0e08385
qovery <contact@qovery.com>
2024-03-11 15:35:35 +0100

    Brew formula update for qovery-cli version v0.84.3

--- Formula/qovery-cli.rb
+++ Formula/qovery-cli.rb
@@ -10,7 +10,7 @@ class QoveryCli < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/Qovery/qovery-cli/releases/download/v0.84.3/qovery-cli_0.84.3_darwin_amd64.tar.gz"
-      sha256 "a5fbd5086d4fb428fa37749804c4e0f3141dd70545fbbbc0b9d971dd110dd10c"
+      sha256 "13b9cb627337c3fb08a0046b8b2d1037762d68f27275cc4343f6781377bc28c1"
 
       def install
         bin.install "qovery"
@@ -18,7 +18,7 @@ class QoveryCli < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/Qovery/qovery-cli/releases/download/v0.84.3/qovery-cli_0.84.3_darwin_arm64.tar.gz"
-      sha256 "9fc825d1f62b283b32ac4e24af6bf626f3759722d31073b088fe921c36186c5e"
+      sha256 "29885099002284c95e7c1ffbcec94bfe56462ea2165828adc866ef456270b903"
 
       def install
         bin.install "qovery"
@@ -27,17 +27,17 @@ class QoveryCli < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Qovery/qovery-cli/releases/download/v0.84.3/qovery-cli_0.84.3_linux_arm64.tar.gz"
-      sha256 "56574481e4369fad257a967bb0b279538ac039d38fce6ea25be835d8cd90e25d"
+    if Hardware::CPU.intel?
+      url "https://github.com/Qovery/qovery-cli/releases/download/v0.84.3/qovery-cli_0.84.3_linux_amd64.tar.gz"
+      sha256 "4a28ca5c0b9af1e93449b48bb646d2cab54efa98b922faaaad96bdd9c52fd029"
 
       def install
         bin.install "qovery"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/Qovery/qovery-cli/releases/download/v0.84.3/qovery-cli_0.84.3_linux_amd64.tar.gz"
-      sha256 "4265188b7448fc9d334ccf7360939980d87182b9a78c07edec42a2bf054f04c4"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/Qovery/qovery-cli/releases/download/v0.84.3/qovery-cli_0.84.3_linux_arm64.tar.gz"
+      sha256 "0ab2f885c188e6f79cb7bcfc7e84f3648e6b3a7e4c2d211bb5da80e8b9aa4acf"
 
       def install
         bin.install "qovery"
